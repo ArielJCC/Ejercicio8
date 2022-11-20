bbc31010e817455ec184a9df9929a202f4c1455e
unknown <xx06102002@gmail.com>
2022-11-20 16:50:28 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Escribir un programa que escriba todos los múltiplos de 3 del número 1 al 3000 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for(int i = 0; i <= 3000; i = i + 3)
+        Console.WriteLine(i);
+    }
+}
